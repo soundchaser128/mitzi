406aa8245d0a4dfb5203659b3f24a7e936f0b66b
soundchaser128 <soundchaser128@gmail.com>
2022-06-04 12:44:28 +0200

    Revert "set image aspect ratio instead of height/width"
    This reverts commit 8b0389f46c9ebd0165a37ff7bade916bc79b309d.

--- app/components/templates/CardTemplate.tsx
+++ app/components/templates/CardTemplate.tsx
@@ -19,7 +19,7 @@ const CardTemplateTier: React.FC<{
         <img
           src={tier.image}
           alt={tier.name}
-          className="aspect-square rounded-t-3xl object-cover object-center"
+          className="h-[400px] w-[350px] rounded-t-3xl object-cover object-center"
         />
 
         <div className="mt-4 px-10">
