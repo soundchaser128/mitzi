428eb4fafd6d688120a977b514d021af1bffad32
soundchaser128 <soundchaser128@gmail.com>
2022-07-14 19:33:59 +0200

    stuff

--- app/routes/banner.tsx
+++ app/routes/banner.tsx
@@ -2,6 +2,9 @@ import {useState} from "react"
 import FileDrop from "~/components/FileDrop"
 import styles from "../styles/styles"
 import Fraction from "fraction.js"
+import clsx from "clsx"
+import {nanoid} from "nanoid"
+import produce from "immer"
 
 const BANNER_ASPECT_RATIO = new Fraction(3, 1)
 
@@ -13,12 +16,50 @@ function calculateAspectRatioForImage(numberOfImages: number): Fraction {
   }
 }
 
+interface Image {
+  url: string
+  position: {x: number; y: number}
+  name: string
+  id: string
+}
+
+interface Settings {
+  darken: boolean
+  text: string
+}
+
+const defaultSettings: Settings = {
+  darken: true,
+  text: "Your Name",
+}
+
 const Index: React.FC = () => {
-  const [files, setFiles] = useState<string[]>([])
+  const [files, setFiles] = useState<Image[]>([])
+  const [settings, setSettings] = useState(defaultSettings)
 
   const onUpload = (uploads: File[]) => {
-    const urls = uploads.map(URL.createObjectURL)
-    setFiles(files.concat(urls))
+    const images = uploads.map((file) => ({
+      url: URL.createObjectURL(file),
+      position: {x: 50, y: 50},
+      name: file.name,
+      id: nanoid(),
+    }))
+    setFiles(files.concat(images))
+  }
+
+  const onFilePositionChange = (
+    index: number,
+    axis: "x" | "y",
+    value: number
+  ) => {
+    const newFiles = produce(files, (draft) => {
+      draft[index].position[axis] = value
+    })
+    setFiles(newFiles)
+  }
+
+  const onChange = (key: keyof Settings, value: any) => {
+    setSettings({...settings, [key]: value})
   }
 
   const imageRatio = calculateAspectRatioForImage(files.length).simplify()
@@ -28,28 +69,113 @@ const Index: React.FC = () => {
     <main className="relative flex min-h-screen bg-white">
       <section className="z-10 flex flex-col bg-indigo-50 p-2 shadow-xl">
         <div className={styles.field}>
+          <h2 className="mb-3 text-xl font-bold">Your name</h2>
+          <input
+            placeholder="Enter your name"
+            type="text"
+            className={styles.input}
+            value={settings.text}
+            onChange={(e) => onChange("text", e.target.value)}
+          />
+        </div>
+        <div className={styles.field}>
+          <h2 className="mb-3 text-xl font-bold">Darken images?</h2>
+          <input
+            type="checkbox"
+            checked={settings.darken}
+            onChange={(e) => onChange("darken", e.target.checked)}
+            title="darken the image?"
+          />
+        </div>
+        <div className={styles.field}>
           <label className={styles.label}>Upload images</label>
           <FileDrop allowMultiple onUpload={onUpload} />
         </div>
+        {files.length > 0 && (
+          <div className={styles.field}>
+            <div className="flex flex-col gap-2 divide-y divide-gray-500">
+              {files.map((file, idx) => (
+                <div key={file.id}>
+                  <p>
+                    File <strong>{file.name}</strong>
+                  </p>
+                  <div className="flex flex-col">
+                    <label>X position</label>
+                    <input
+                      min="0"
+                      max="100"
+                      title="Adjust image X position"
+                      type="range"
+                      value={file.position.x}
+                      onChange={(event) =>
+                        onFilePositionChange(
+                          idx,
+                          "x",
+                          event.target.valueAsNumber
+                        )
+                      }
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label>Y position</label>
+                    <input
+                      min="0"
+                      max="100"
+                      title="Adjist image Y position"
+                      type="range"
+                      value={file.position.y}
+                      onChange={(event) =>
+                        onFilePositionChange(
+                          idx,
+                          "y",
+                          event.target.valueAsNumber
+                        )
+                      }
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </section>
-      <section className="grow bg-white">
-        <div className="flex min-h-screen w-full items-center justify-center bg-white">
+      <section className="grow bg-neutral-500">
+        <div className="flex min-h-screen w-full items-center justify-center">
           <div
             id="banner-frame"
-            className="container flex h-auto max-w-full grid-flow-row"
+            className={clsx(
+              "container relative flex h-auto max-w-full grid-flow-row bg-white"
+              // [withPadding && "gap-2 p-2"]
+            )}
             style={{
               aspectRatio: "3 / 1",
             }}
           >
-            {files.map((file, idx) => (
+            {files.length === 0 && (
+              <div className="flex h-full w-full items-center justify-center text-xl">
+                Upload some images to get started.
+              </div>
+            )}
+            {files.map((image, idx) => (
               <img
-                className="object-cover"
-                src={file}
+                className={clsx(
+                  "object-cover",
+                  settings.darken && "brightness-75"
+                )}
+                src={image.url}
                 key={idx}
                 alt="user-uploaded data"
-                style={{aspectRatio}}
+                style={{
+                  aspectRatio,
+                  objectPosition: `${image.position.x}% ${image.position.y}%`,
+                }}
               />
             ))}
+            {files.length > 0 && (
+              <h1 className="absolute top-0 right-0 z-10 flex h-full w-full items-center justify-center text-8xl font-bold text-white">
+                {settings.text}
+              </h1>
+            )}
           </div>
         </div>
       </section>
