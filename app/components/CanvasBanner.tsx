13a926f193279c501f0e1b6658673cc5f0bc8653
soundchaser128 <soundchaser128@gmail.com>
2022-07-24 08:46:48 +0200

    fix aspect ratio calculations

--- app/components/CanvasBanner.tsx
+++ app/components/CanvasBanner.tsx
@@ -1,6 +1,7 @@
 import Fraction from "fraction.js"
-import {useEffect, useMemo, useRef} from "react"
+import {useEffect, useRef} from "react"
 import type {BannerProps} from "./Banner"
+import ImageCropModal from "./ImageCropModal"
 
 const CanvasBanner: React.FC<BannerProps> = ({
   files,
@@ -10,7 +11,9 @@ const CanvasBanner: React.FC<BannerProps> = ({
   setModalImage,
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
-
+  const imageAspectRatio = new Fraction(settings.aspectRatio).div(
+    files.length === 0 ? 1 : files.length
+  )
   useEffect(() => {
     if (!canvasRef.current) {
       return
@@ -21,17 +24,18 @@ const CanvasBanner: React.FC<BannerProps> = ({
       return
     }
 
-    const width = canvasRef.current?.clientWidth
-    const height = canvasRef.current?.clientHeight
+    const width = canvasRef.current.clientWidth
+    const height = canvasRef.current.clientHeight
+    canvasRef.current.width = width
+    canvasRef.current.height = height
+
+    ctx.imageSmoothingEnabled = true
+    ctx.imageSmoothingQuality = "high"
+
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, width, height)
 
     const imageWidth = Math.floor(width / files.length)
-    // const imageHeight = height
-
-    const imageAspectRatio = new Fraction(settings.aspectRatio).div(
-      files.length === 0 ? 1 : files.length
-    )
 
     files.forEach((image, idx) => {
       const xPosition = idx * imageWidth
@@ -40,15 +44,16 @@ const CanvasBanner: React.FC<BannerProps> = ({
       img.src = image.url
 
       img.onload = (e) => {
-        const sourceX = 0
-        const sourceY = 0
-        const sourceWidth = imageAspectRatio
-        const sourceHeight = img.height
+        const sourceX = image.crop?.x || 0
+        const sourceY = image.crop?.y || 0
+        let sourceWidth = img.width
+        let sourceHeight = img.height
 
-        console.log({sourceWidth, sourceHeight})
-
-        ctx.imageSmoothingEnabled = true
-        ctx.imageSmoothingQuality = "high"
+        if (img.width > img.height) {
+          sourceWidth = img.height * imageAspectRatio.valueOf()
+        } else {
+          sourceHeight = img.width * imageAspectRatio.inverse().valueOf()
+        }
 
         ctx.drawImage(
           img,
@@ -62,18 +67,28 @@ const CanvasBanner: React.FC<BannerProps> = ({
           height
         )
       }
-
-      // document.removeChild(img)
+      img.remove()
     })
-  }, [canvasRef, files])
+  }, [canvasRef, files, settings.aspectRatio, imageAspectRatio])
 
   return (
-    <canvas
-      ref={canvasRef}
-      id="banner-frame"
-      className="w-full"
-      style={{aspectRatio: "3 / 1"}}
-    />
+    <>
+      <ImageCropModal
+        onSave={onChangeImageCrop}
+        isOpen={Boolean(modalImage)}
+        closeModal={() => setModalImage(null)}
+        imageBlobUrl={modalImage?.url}
+        imageId={modalImage?.id}
+        title="Edit image"
+        aspectRatio={imageAspectRatio.valueOf()}
+      />
+      <canvas
+        ref={canvasRef}
+        id="banner-frame"
+        className="w-full"
+        style={{aspectRatio: settings.aspectRatio}}
+      />
+    </>
   )
 }
 
