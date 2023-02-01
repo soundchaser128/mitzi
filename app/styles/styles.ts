c201d937e0a312eca3341f0a4fce0d0c9ac59524
soundchaser128 <soundchaser128@gmail.com>
2023-02-01 23:01:19 +0100

    update deps

--- app/styles/styles.ts
+++ app/styles/styles.ts
@@ -2,7 +2,7 @@ const styles = {
   label: "label",
   input: "grow input input-sm input-primary",
   field: "form-control",
-  formHeader: "text-xl font-bold",
+  formHeader: "text-xl font-bold text-primary",
 }
 
 export default styles
